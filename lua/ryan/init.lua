508d96c75927037e92c9f2ae5345fd0e2bde1c8a
Ryan Kopp <ryck574@gmail.com>
2023-10-10 19:22:34 -0700

    initial commit

---
+++ lua/ryan/init.lua
@@ -0,0 +1,2 @@
+require("ryan.set")
+require("ryan.remap")
---
+++ lua/ryan/set.lua
@@ -0,0 +1,33 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 12
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+
+if vim.fn.expand("%:e") == "h" then
+    nvim_buf_set_option(0, "filetype", "c")
+end
