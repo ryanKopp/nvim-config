1b7bb0f1d927380f4e6bce61504ae89c3ed853bc
Ryan Kopp <ryck574@gmail.com>
2023-11-11 14:22:02 -0700

    Fix telescope with update

--- lua/ryan/packer.lua
+++ lua/ryan/packer.lua
@@ -6,12 +6,18 @@ return require('packer').startup(function(use)
     use 'navarasu/onedark.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
+    use
+    {'nvim-treesitter/nvim-treesitter',
+    run = function()
+        local ts_update = require('nvim-treesitter.install').update({ with_sync = true})
+        ts_update()
+    end,}
+    --run = ':TSUpdate'})
 
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
